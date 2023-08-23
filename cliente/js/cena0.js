2d0a991e85013a563c7784a83e4beb61b8bd054a
TheFBH <matheus.s14@aluno.ifsc.edu.br>
2023-08-23 20:20:35 +0000

    Configuração do ESLint

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -1,16 +1,15 @@
 export default class cena0 extends Phaser.Scene {
-
   constructor () {
     super('cena0')
   }
 
-  preload() {
+  preload () {
     this.load.image('ifsc-sj-2014', '../assets/imagens/ifsc-sj-2014.png')
   }
 
-  create() {
+  create () {
     this.add.image(400, 225, 'ifsc-sj-2014')
   }
 
-  countdown() { }
+  countdown () { }
 }
