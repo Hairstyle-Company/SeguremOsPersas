a9f92fedf1364126d0cf443a944c0ed308e7b6a0
Natanxtx Silva <natanielsilvahames@gmail.com>
2023-08-30 20:11:40 +0000

    bobeira

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -8,7 +8,7 @@ export default class cena0 extends Phaser.Scene {
     this.load.image('ifsc-sj-2014', '../assets/imagens/ifsc-sj-2014.png')
 
     /* Imagem do personagem */
-    this.load.spritesheet('derek', '../assets/imagens/Derek.png', {
+    this.load.spritesheet('xerxes', '../assets/imagens/xerxes.png', {
       frameWidth: 64,
       frameHeight: 64
     })
@@ -38,27 +38,27 @@ export default class cena0 extends Phaser.Scene {
     create() {
       this.add.image(400, 225, 'ifsc-sj-2014')
 
-      this.personagem = this.physics.add.sprite(400, 255, 'derek')
+      this.personagem = this.physics.add.sprite(400, 255, 'xerxes')
         .setInteractive()
         .on('pointerdown', () => {
-          this.personagem.anims.play('derek-direita')
+          this.personagem.anims.play('xerxes-direita')
           this.personagem.setVelocityX(100)
         })
       
       /* Animação parado*/
       this.anims.create({
-        key: 'derek-parado',
-        frames: this.anims.generateFrameNumbers('derek', {
-          start: 0,
-          end: 0
+        key: 'xerxes-parado',
+        frames: this.anims.generateFrameNumbers('xerxes', {
+          start: 5,
+          end: 6
         }),
         frameRate: 1
       })
 
       /* Animação andar direita */
       this.anims.create({
-        key: 'derek-direita',
-        frames: this.anims.generateFrameNumbers('derek', {
+        key: 'xerxes-direita',
+        frames: this.anims.generateFrameNumbers('xerxes', {
           start: 8,
           end: 11
         }),
@@ -68,8 +68,8 @@ export default class cena0 extends Phaser.Scene {
 
       /* Animação andar esquerda */
       this.anims.create({
-        key: 'derek-esquerda',
-        frames: this.anims.generateFrameNumbers('derek', {
+        key: 'xerxes-esquerda',
+        frames: this.anims.generateFrameNumbers('xerxes', {
           start: 4,
           end: 7
         }),
@@ -79,8 +79,8 @@ export default class cena0 extends Phaser.Scene {
 
       /* Animação andar cima */
       this.anims.create({
-        key: 'derek-cima',
-        frames: this.anims.generateFrameNumbers('derek', {
+        key: 'xerxes-cima',
+        frames: this.anims.generateFrameNumbers('xerxes', {
           start: 12,
           end: 15
         }),
@@ -90,8 +90,8 @@ export default class cena0 extends Phaser.Scene {
 
       /* Animação andar baixo */
       this.anims.create({
-        key: 'derek-baixo',
-        frames: this.anims.generateFrameNumbers('derek', {
+        key: 'xerxes-baixo',
+        frames: this.anims.generateFrameNumbers('xerxes', {
           start: 0,
           end: 3
         }),
@@ -104,12 +104,12 @@ export default class cena0 extends Phaser.Scene {
         .setInteractive()
         .on('pointerdown', () => {
           this.direita.setFrame(1)
-          this.personagem.anims.play('derek-direita', true)
+          this.personagem.anims.play('xerxes-direita', true)
           this.personagem.setVelocityX(100)
         })
         .on('pointerup', () => {
           this.direita.setFrame(0)
-          this.personagem.anims.play('derek-parado')
+          this.personagem.anims.play('xerxes-parado')
           this.personagem.setVelocityX(0)
         })
       
@@ -118,12 +118,12 @@ export default class cena0 extends Phaser.Scene {
         .setInteractive()
         .on('pointerdown', () => {
           this.esquerda.setFrame(1)
-          this.personagem.anims.play('derek-esquerda', true)
+          this.personagem.anims.play('xerxes-esquerda', true)
           this.personagem.setVelocityX(-100)
         })
         .on('pointerup', () => {
           this.esquerda.setFrame(0)
-          this.personagem.anims.play('derek-parado')
+          this.personagem.anims.play('xerxes-parado')
           this.personagem.setVelocityX(0)
         })
       
@@ -132,12 +132,12 @@ export default class cena0 extends Phaser.Scene {
         .setInteractive()
         .on('pointerdown', () => {
           this.cima.setFrame(1)
-          this.personagem.anims.play('derek-cima', true)
+          this.personagem.anims.play('xerxes-cima', true)
           this.personagem.setVelocityY(-100)
         })
         .on('pointerup', () => {
           this.cima.setFrame(0)
-          this.personagem.anims.play('derek-parado')
+          this.personagem.anims.play('xerxes-parado')
           this.personagem.setVelocityY(0)
         })
 
@@ -146,12 +146,12 @@ export default class cena0 extends Phaser.Scene {
         .setInteractive()
         .on('pointerdown', () => {
           this.baixo.setFrame(1)
-          this.personagem.anims.play('derek-baixo', true)
+          this.personagem.anims.play('xerxes-baixo', true)
           this.personagem.setVelocityY(100)
         })
         .on('pointerup', () => {
           this.baixo.setFrame(0)
-          this.personagem.anims.play('derek-parado')
+          this.personagem.anims.play('xerxes-parado')
           this.personagem.setVelocityY(0)
         })
 
