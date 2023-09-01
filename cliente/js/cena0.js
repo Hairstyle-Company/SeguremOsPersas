d709fb1df0140538b61c2c035700b8b7f35b515c
TheFBH . <matheus.s14@aluno.ifsc.edu.br>
2023-09-01 20:26:13 +0000

    Programação do Joystick

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -8,154 +8,205 @@ export default class cena0 extends Phaser.Scene {
     this.load.image('ifsc-sj-2014', '../assets/imagens/ifsc-sj-2014.png')
 
     /* Imagem do personagem */
-    this.load.spritesheet('xerxes', '../assets/imagens/xerxes.png', {
-      frameWidth: 64,
+    this.load.spritesheet('player1', '../assets/imagens/player1.png', {
+      frameWidth: 40,
       frameHeight: 64
     })
 
     /* Imagem dos botões */
-    this.load.spritesheet('direita', '../assets/imagens/direita.png', {
-      frameWidth: 64,
-      frameHeight: 64
-    })
+    this.load.image('vazio', '../assets/imagens/vazio.png')
+    this.load.image('circulo', '../assets/imagens/circulo.png')
 
-    this.load.spritesheet('esquerda', '../assets/imagens/esquerda.png', {
-      frameWidth: 64,
-      frameHeight: 64
-    })
-
-    this.load.spritesheet('baixo', '../assets/imagens/baixo.png', {
-      frameWidth: 64,
-      frameHeight: 64
-    })
-
-    this.load.spritesheet('cima', '../assets/imagens/cima.png', {
-      frameWidth: 64,
-      frameHeight: 64
-    })
   }
 
-    create() {
-      this.add.image(400, 225, 'ifsc-sj-2014')
+  create () {
+    this.add.image(400, 225, 'ifsc-sj-2014')
 
-      this.personagem = this.physics.add.sprite(400, 255, 'xerxes')
-        .setInteractive()
-        .on('pointerdown', () => {
-          this.personagem.anims.play('xerxes-direita')
-          this.personagem.setVelocityX(100)
-        })
-      
-      /* Animação parado*/
-      this.anims.create({
-        key: 'xerxes-parado',
-        frames: this.anims.generateFrameNumbers('xerxes', {
-          start: 5,
-          end: 6
-        }),
-        frameRate: 1
+    this.personagem = this.physics.add.sprite(400, 255, 'player1')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.personagem.anims.play('player1-direita')
+        this.personagem.setVelocityX(100)
       })
 
-      /* Animação andar direita */
-      this.anims.create({
-        key: 'xerxes-direita',
-        frames: this.anims.generateFrameNumbers('xerxes', {
-          start: 8,
-          end: 11
-        }),
-        frameRate: 6,
-        repeat: -1
+    /* Animação parado*/
+    this.anims.create({
+      key: 'player1-parado',
+      frames: this.anims.generateFrameNumbers('player1', {
+        start: 0,
+        end: 3
+      }),
+      frameRate: 15,
+      repeat: -1
+    })
+
+    /* Animação andar direita */
+    this.anims.create({
+      key: 'player1-direita',
+      frames: this.anims.generateFrameNumbers('player1', {
+        start: 0,
+        end: 3
+      }),
+      frameRate: 15,
+      repeat: -1
+    })
+
+    /* Animação andar esquerda */
+    this.anims.create({
+      key: 'player1-esquerda',
+      frames: this.anims.generateFrameNumbers('player1', {
+        start: 0,
+        end: 3
+      }),
+      frameRate: 15,
+      repeat: -1
+    })
+
+    /* Animação andar cima */
+    this.anims.create({
+      key: 'player1-cima',
+      frames: this.anims.generateFrameNumbers('player1', {
+        start: 0,
+        end: 3
+      }),
+      frameRate: 15,
+      repeat: -1
+    })
+
+    /* Animação andar baixo */
+    this.anims.create({
+      key: 'player1-baixo',
+      frames: this.anims.generateFrameNumbers('player1', {
+        start: 0,
+        end: 3
+      }),
+      frameRate: 15,
+      repeat: -1
+    })
+
+    /*Joystick*/
+    this.add.image(125, 325, 'circulo')
+
+    /* Programação do botão direito */
+    this.direita = this.physics.add.sprite(200, 325, 'vazio')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.direita.setFrame(1)
+        this.personagem.anims.play('player1-direita', true)
+        this.personagem.setVelocityX(100)
+      })
+      .on('pointerup', () => {
+        this.direita.setFrame(0)
+        this.personagem.anims.play('player1-parado')
+        this.personagem.setVelocityX(0)
       })
 
-      /* Animação andar esquerda */
-      this.anims.create({
-        key: 'xerxes-esquerda',
-        frames: this.anims.generateFrameNumbers('xerxes', {
-          start: 4,
-          end: 7
-        }),
-        frameRate: 6,
-        repeat: -1
+    /* Programação do botão esquerdo */
+    this.esquerda = this.physics.add.sprite(50, 325, 'vazio')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.esquerda.setFrame(1)
+        this.personagem.anims.play('player1-esquerda', true)
+        this.personagem.setVelocityX(-100)
+      })
+      .on('pointerup', () => {
+        this.esquerda.setFrame(0)
+        this.personagem.anims.play('player1-parado')
+        this.personagem.setVelocityX(0)
       })
 
-      /* Animação andar cima */
-      this.anims.create({
-        key: 'xerxes-cima',
-        frames: this.anims.generateFrameNumbers('xerxes', {
-          start: 12,
-          end: 15
-        }),
-        frameRate: 6,
-        repeat: -1
+    /* Programação do botão cima */
+    this.cima = this.physics.add.sprite(125, 250, 'vazio')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.cima.setFrame(1)
+        this.personagem.anims.play('player1-cima', true)
+        this.personagem.setVelocityY(-100)
+      })
+      .on('pointerup', () => {
+        this.cima.setFrame(0)
+        this.personagem.anims.play('player1-parado')
+        this.personagem.setVelocityY(0)
       })
 
-      /* Animação andar baixo */
-      this.anims.create({
-        key: 'xerxes-baixo',
-        frames: this.anims.generateFrameNumbers('xerxes', {
-          start: 0,
-          end: 3
-        }),
-        frameRate: 6,
-        repeat: -1
+    /* Programação do botão baixo */
+    this.baixo = this.physics.add.sprite(125, 400, 'vazio')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.baixo.setFrame(1)
+        this.personagem.anims.play('player1-baixo', true)
+        this.personagem.setVelocityY(100)
+      })
+      .on('pointerup', () => {
+        this.baixo.setFrame(0)
+        this.personagem.anims.play('player1-parado')
+        this.personagem.setVelocityY(0)
       })
 
-      /* Programação do botão direito */
-      this.direita = this.add.sprite(250, 400, 'direita')
-        .setInteractive()
-        .on('pointerdown', () => {
-          this.direita.setFrame(1)
-          this.personagem.anims.play('xerxes-direita', true)
-          this.personagem.setVelocityX(100)
-        })
-        .on('pointerup', () => {
-          this.direita.setFrame(0)
-          this.personagem.anims.play('xerxes-parado')
-          this.personagem.setVelocityX(0)
-        })
-      
-      /* Programação do botão esquerdo */
-      this.esquerda = this.add.sprite(100, 400, 'esquerda')
-        .setInteractive()
-        .on('pointerdown', () => {
-          this.esquerda.setFrame(1)
-          this.personagem.anims.play('xerxes-esquerda', true)
-          this.personagem.setVelocityX(-100)
-        })
-        .on('pointerup', () => {
-          this.esquerda.setFrame(0)
-          this.personagem.anims.play('xerxes-parado')
-          this.personagem.setVelocityX(0)
-        })
-      
-      /* Programação do botão cima */
-      this.cima = this.add.sprite(175, 325, 'cima')
-        .setInteractive()
-        .on('pointerdown', () => {
-          this.cima.setFrame(1)
-          this.personagem.anims.play('xerxes-cima', true)
-          this.personagem.setVelocityY(-100)
-        })
-        .on('pointerup', () => {
-          this.cima.setFrame(0)
-          this.personagem.anims.play('xerxes-parado')
-          this.personagem.setVelocityY(0)
-        })
+    /* Programação do botão Diagonal baixo-esquerda */
+    this.baixo = this.physics.add.sprite(75, 375, 'vazio')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.baixo.setFrame(1)
+        this.personagem.anims.play('player1-baixo', true)
+        this.personagem.setVelocityY(100)
+        this.personagem.setVelocityX(-100)
+      })
+      .on('pointerup', () => {
+        this.baixo.setFrame(0)
+        this.personagem.anims.play('player1-parado')
+        this.personagem.setVelocityY(0)
+        this.personagem.setVelocityX(0)
+      })
 
-      /* Programação do botão baixo */
-      this.baixo = this.add.sprite(175, 400, 'baixo')
-        .setInteractive()
-        .on('pointerdown', () => {
-          this.baixo.setFrame(1)
-          this.personagem.anims.play('xerxes-baixo', true)
-          this.personagem.setVelocityY(100)
-        })
-        .on('pointerup', () => {
-          this.baixo.setFrame(0)
-          this.personagem.anims.play('xerxes-parado')
-          this.personagem.setVelocityY(0)
-        })
+    /* Programação do botão Diagonal baixo-direita */
+    this.baixo = this.physics.add.sprite(175, 375, 'vazio')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.baixo.setFrame(1)
+        this.personagem.anims.play('player1-baixo', true)
+        this.personagem.setVelocityY(100)
+        this.personagem.setVelocityX(100)
+      })
+      .on('pointerup', () => {
+        this.baixo.setFrame(0)
+        this.personagem.anims.play('player1-parado')
+        this.personagem.setVelocityY(0)
+        this.personagem.setVelocityX(0)
+      })
 
-    }
+    /* Programação do botão diagonal cima-esquerda */
+    this.cima = this.physics.add.sprite(75, 275, 'vazio')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.cima.setFrame(1)
+        this.personagem.anims.play('player1-cima', true)
+        this.personagem.setVelocityY(-100)
+        this.personagem.setVelocityX(-100)
+      })
+      .on('pointerup', () => {
+        this.cima.setFrame(0)
+        this.personagem.anims.play('player1-parado')
+        this.personagem.setVelocityY(0)
+        this.personagem.setVelocityX(0)
+      })
 
-    countdown() { }
+    /* Programação do botão diagonal cima-direita */
+    this.cima = this.physics.add.sprite(175, 275, 'vazio')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.cima.setFrame(1)
+        this.personagem.anims.play('player1-cima', true)
+        this.personagem.setVelocityY(-100)
+        this.personagem.setVelocityX(100)
+      })
+      .on('pointerup', () => {
+        this.cima.setFrame(0)
+        this.personagem.anims.play('player1-parado')
+        this.personagem.setVelocityY(0)
+        this.personagem.setVelocityX(0)
+      })
   }
+
+  countdown () { }
+}
