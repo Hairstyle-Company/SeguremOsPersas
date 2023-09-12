ef8bc8c2910e50616467bd2335e8f28408fa0208
TheFBH . <matheus.s14@aluno.ifsc.edu.br>
2023-09-12 18:10:40 +0000

    Tentativa de fazer alguma coisa (Fracasso)

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -6,9 +6,16 @@ export default class cena0 extends Phaser.Scene {
   preload () {
     /* Imagem de fundo */
     this.load.image('menu-inicial', '../assets/imagens/menu-inicial.png')
+    this.load.image('vazio', '../assets/imagens/vazio.png')
+    this.load.spritesheet('tela_cheia', '../assets/botão/telacheia.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
   }
     /* Adicionar mapa/player*/
   create () {
     this.add.image(400, 225, 'menu-inicial')
-    }
-  }
+    this.physics.add.spryte(400, 255, 'vazio')
+
+
+}
