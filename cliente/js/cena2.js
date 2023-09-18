72609b7ee8f5b90f9e66eb9205dc1afa4b878620
TheFBH . <matheus.s14@aluno.ifsc.edu.br>
2023-09-18 20:00:43 +0000

    ajuste nos textos

--- cliente/js/cena2.js
+++ cliente/js/cena2.js
@@ -10,12 +10,27 @@ export default class cena2 extends Phaser.Scene {
 
   create () {
     this.add.image(400, 225, 'menu-sala')
+
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.game.scene.stop('cena2')
+        this.game.scene.start('cena3')
+      })
+
     this.add.text(400, 225,
-      'Há muitos anos atrás, rolou uma\nguerra do balacobaco mt louca slc',
+      '\tHá muitos anos atrás, o reino dos Persas seguia em uma rápida expanção \n territorial, subjulgando as cidades dominadas, escravizando pessoas e \ncobrando abusivos impostos.',
       {
         fontFamily: 'PressStart2P',
-        fontSize: '15px',
-        fill: 'white'
-      }).setOrigin(0.5, 1.0)
+        fontSize: '10px',
+        fill: 'yellow'
+      }).setOrigin(0.5, 2.0)
+
+    this.add.text(400, 225,
+      '\t Agora mesmo, Xerxes, o rei dos Persas, reúne as suas tropas para atacar, \n um contingente de 250 mil homens. Esparta e seus aliados reúnem as pressas \n 7 mil soldados, porém outros povos Gregos ainda precisavam de mais tempo \n para juntar seus exércitos...eles precisavam comprar tempo para isso.',
+      {
+        fontFamily: 'PressStart2P',
+        fontSize: '10px',
+        fill: 'yellow'
+      }).setOrigin(0.5, -1.0)
   }
 }
